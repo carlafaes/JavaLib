66c4fbb951dc08ad1be6cf3e075ca2b6251a9bac
carlafaes <carlafaes18@hotmail.com>
2023-02-16 16:15:18 -0300

    herencia y polimorfismo

--- src/libroJava/Fecha.java
+++ src/libroJava/Fecha.java
@@ -11,6 +11,24 @@ public class Fecha {
     private int mes;
     private int año;
     
+    public Fecha(String s){
+        //busco la primera ocurrencia de "/"
+        int pos1=s.indexOf("/");
+        //busco la ultima ocurrencia de "/"
+        int pos2=s.lastIndexOf("/");
+        //proceso el dia
+        String sDia=s.substring(0,pos1);
+        //proceso el mes 
+        String sMes=s.substring(pos1+1,pos2);
+        mes=Integer.parseInt(sMes);
+        //proceso el año
+        String sAño=s.substring(pos2+1);
+        año=Integer.parseInt(sAño);
+    }
+
+    Fecha() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     public int getDia(){
         //retorna el valor de la variable dia
         return dia;
@@ -30,5 +48,36 @@ public class Fecha {
     }
     public void setAño(int año){
         this.año=año;
+    }
+    private int fechaToDias(){
+        return año*360+mes*30+dia;
+    }
+    //asigna la fecha expresada en dias a los atributos
+    private void diasToFecha(int i){
+        //dividimos por 360 y obtenemos el año
+        año=(int)i/360;
+        //del resto de la division anterior podremos obtener el mes y el dia
+        int resto=i%360;
+        //el mes es el resto dividido 30
+        mes=(int) resto/30;
+        //el resto de la division anterior son los dias
+        dia=resto%30;
+        //ajuste por si dia quedo en cero
+        if(dia==0){
+            dia=30;
+            mes--;
+        }
+        //ajuste por si el mes quedo en cero
+        if(mes==0){
+            mes=12;
+            año--;
+        }
+    }
+    public void addDias(int d){
+        //convierto la fecha a dias y le sumo d
+        int sum=fechaToDias()+d;
+        
+        //la fecha resultante (sum) la separo en dia,mes y año
+        diasToFecha(sum);
     }
 }
---
+++ src/libroJava/FechaDetallada.java
@@ -0,0 +1,13 @@
+package libroJava;
+
+/**La clase Fecha nos es util,funciona bien y es practica. Sin embargo, queremos modificar la forma en la que una fecha
+ * se representa a si misma cuando le invocamos el metodo toString.
+ * La solucion sera crear una nueva clase que herede de Fecha y que modifique la manera en la que esta se representa como cadena.
+ * Esto lo podremos hacer sobrescribiendo el metodo toString. Llamaremos a la nueva clase FechaDetallada y haremos 
+ * que se represente asi:"25 de Octubre de 2009".
+ *
+ * @author car
+ */
+public class FechaDetallada {
+    
+}
